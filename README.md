0bace7dda8f02fcfcaab948aa9d09ded7cea5478
chlouzada <chlouzada@gmail.com>
2023-09-10 19:17:27 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Firebase Deploy
+
+Firebase Deploy streamlines the use of the `firebase deploy` command by enabling you to pre-answer prompts. This is especially useful when you want to automate Firebase deployments within scripts or automated workflows.
+
+## Install
+
+```bash
+npm install -g firebase-deploy
+```
---
+++ src/index.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+// @ts-check
+
+import { Command } from 'commander';
+import { spawn } from 'child_process';
+
+const program = new Command();
+
+program
+  .name('Firebase Deploy')
+  .description('Wrapper for `firebase deploy` that allows preanswering prompts')
+  .version('0.0.0');
+
+program
+  .option(
+    '--confirm-on-deletion',
+    'Automatically confirm deletion of resources if not present on codebase'
+  )
+  .option(
+    '--no-confirm-on-deletion',
+    'Automatically deny deletion of resources if not present on codebase'
+  )
+  .option(
+    '--confirm-on-retry-failure',
+    'Automatically confirm retrying functions in case of failure'
+  )
+  .option(
+    '--no-confirm-on-retry-failure',
+    'Automatically deny retrying functions in case of failure'
+  );
+
+program.parse();
+
+main(program.opts());
+
+/**
+ * @param {{
+ *  confirmOnDeletion: boolean | undefined;
+ *  confirmOnRetryFailure: boolean | undefined;
+ * }} options
+ * @returns {void}
+ * */
+function main({ confirmOnDeletion, confirmOnRetryFailure }) {
+  const command = 'firebase';
+  const args = ['deploy', '--interactive'];
+
+  const child = spawn(command, args, {
+    stdio: 'pipe',
+  });
+
+  const prompts = [
+    {
+      text: 'Would you like to proceed with deletion? Selecting no will continue the rest',
+      options: ['--confirm-on-deletion', '--no-confirm-on-deletion'],
+      value: confirmOnDeletion,
+    },
+    {
+      text: 'The following functions will newly be retried in case of failure',
+      options: ['--confirm-on-retry-failure', '--no-confirm-on-retry-failure'],
+      value: confirmOnRetryFailure,
+    },
+  ];
+
+  child.stdout.on('data', (data) => {
+    const output = data.toString();
+    console.log(output);
+
+    prompts.forEach((prompt) => {
+      if (output.includes(prompt.text)) {
+        if (prompt.value === undefined) {
+          console.log(
+            `Prompt '${
+              prompt.text
+            }' was found but no response was provided. Please provide either ${prompt.options.join(
+              ' or '
+            )}`
+          );
+          process.exit(1);
+        }
+        child.stdin.write(prompt.value ? 'y\n' : 'n\n');
+      }
+    });
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(data.toString());
+  });
+
+  child.on('close', (code) => {
+    console.log(`process exited with code ${code}`);
+  });
+}
